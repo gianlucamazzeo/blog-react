fd63ae1b9a18005a36ece48e92efb30ecffb606c
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-09-20 22:02:30 +0200

    role of reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -36,9 +36,10 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 
 // Finally, perfect correct way!!
+
   export const fetchPosts = () => async dispatch => {
      const response = await jsonPlaceholder.get('/posts');
-        dispatch({type: 'FETCH_POSTS', response })
+        dispatch({type: 'FETCH_POSTS', payload: response })
       
    };
  
