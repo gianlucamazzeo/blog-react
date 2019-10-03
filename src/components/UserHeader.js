e624f0d34ec70aa2471b2f14f4bcaa476db4c666
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-10-03 11:22:12 +0200

    add ownProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,8 +7,8 @@ class UserHeader extends React.Component {
         this.props.fetchUser(this.props.userId);
     }
     render() {
-        const user = this.props.users.find(user => user.id === this.props.userId);
-        
+        const {user } = this.props;
+       
         if(!user) {
             return null;
         }
@@ -17,9 +17,9 @@ class UserHeader extends React.Component {
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
 
-    return {users: state.users};
+    return {user: state.users.find(user => user.id === ownProps.userId)};
 
 };
 
